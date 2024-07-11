65fd3fcf544f2954fa5074bb4ac7ccc15dc13870
Hongy <hongyvliang@outlook.com>
2024-07-11 18:14:40 +0800

    添加了《C++ Primer》习题集

---
+++ Practice_1/Chapter_2/6_Permutation/Permutation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int tmp1,tmp2;
+int a[10] = {0}; // a存储三个三位数的各个数位
+
+int main()
+{
+    for(int i = 123;i <= 329;i++)
+    {
+        tmp1 = 2 * i,tmp2 = 3 * i;
+    }
+    return 0;
+}
